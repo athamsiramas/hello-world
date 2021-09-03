2520a655a2bd6281494115ff71b6f98934922b2b
Amnart Thamsiramas <amthamsi@redhat.com>
2021-09-03 12:41:49 +0700

    first commit

---
+++ src/main/java/com/example/howtodoinjava/hellodocker/HelloDockerRestController.java
@@ -0,0 +1,24 @@
+package com.example.howtodoinjava.hellodocker;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+class HelloDockerRestController {
+
+    @Value("${service.version}")
+    String version;
+
+    @RequestMapping("/hello/{name}")
+    public String helloDocker(@PathVariable(value = "name") String name) {
+        JSONObject response = new JSONObject();
+        response.put("message", "Hello " + name + " Response received on : " + new Date());
+        response.put("version", version);
+        return response.toString();
+    }
+}
